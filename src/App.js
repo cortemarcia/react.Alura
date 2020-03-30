b7ee62ff99169455245b1e8b4a8390a939dfddf4
Marcia <cortemarcia80@gmail.com>
2020-03-30 18:08:09 -0300

    Add form on project

--- src/App.js
+++ src/App.js
@@ -1,59 +1,71 @@
-import React,{Component} from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import Tabela from './Tabelas';
+import Form from './Formulario';
 
 
 
 class App extends Component {
- state= {
-  autores:[
-    {
-    nome: 'Marcia',
-    livro: 'React',
-    preco: '1000'
-  },
-  {
-    nome: 'Diogo',
-    livro: '.Net',
-    preco: '367'
-  },
-  {
-    nome: 'Gabriel',
-    livro: 'JAVA',
-    preco: '234'
-  },
-  {
-    nome: 'Leonam',
-    livro: 'P.O',
-    preco: '432'
+  state = {
+    autores: [
+      {
+        nome: 'Marcia',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Diogo',
+        livro: '.Net',
+        preco: '367'
+      },
+      {
+        nome: 'Gabriel',
+        livro: 'JAVA',
+        preco: '234'
+      },
+      {
+        nome: 'Leonam',
+        livro: 'P.O',
+        preco: '432'
+      }
+    ],
+
   }
-  ],
 
- }
+  escutadorDeSubmit=autor =>{
+    
+    console.log(this.state.autores)
+    console.log("...")
+    console.log(...this.state.autores)
+    this.setState({autores: [...this.state.autores,autor]})
 
- removeAuthor = index =>{
-   const {autores}= this.state;
+}    
 
-   this.setState(
-     {
-       autores:autores.filter((autor, posAtual) =>{
-       
-         return posAtual !== index
+  removeAuthor = index => {
+    const { autores } = this.state;
 
-       }),
-     }
-   );
-   
- }
+    this.setState(
+      {
+        autores: autores.filter((autor, posAtual) => {
 
- render(){
-  return(
-    <div className= "app">
-      <Tabela autores = {this.state.autores} removeAuthor={this.removeAuthor} />
-      </div>
+          return posAtual !== index
+
+        }),
+      }
+    );
+
+  }
+
+  render() {
+    return (
+      <Fragment>
+      <Tabela autores={this.state.autores} removeAuthor={this.removeAuthor} />
+      <Form  escutadorDeSubmit ={this.escutadorDeSubmit}/>
+      </Fragment >
+      
   );
- }
- 
+  }
+
 }
- 
+
 export default App;
